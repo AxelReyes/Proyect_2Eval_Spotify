ea345afe8d0016b766724e5e1535068d89ee27c4
01Jesus01 <manuel.jesus64@yahoo.com>
2024-05-28 23:49:43 -0600

    Login funcionado , problema con la pagina administrador, rediseño de play

--- Proyecto/HeroSound/forms.py
+++ Proyecto/HeroSound/forms.py
@@ -1,24 +1,53 @@
 from django import forms
-from django.core import validators
-from HeroSound.models import Cancion
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.validators import MaxLengthValidator
+from HeroSound.models import Cancion, Perfil
 
 class FormUser(forms.Form):
-    name = forms.CharField(validators=[validators.MaxLengthValidator(5)])
+    name = forms.CharField(validators=[MaxLengthValidator(5)])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-                                 widget=forms.HiddenInput)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
     
-    #Función para verificar que los datos en atrapabots están bien
     def clean_botcatcher(self):
         atrapador = self.cleaned_data['botcatcher']
-        if (len(atrapador) > 0):
+        if len(atrapador) > 0:
             raise forms.ValidationError("Ese mi EL BOT!")
         return atrapador
 
+class RegistroForm(forms.ModelForm):
+    username = forms.CharField(label='Username', max_length=150, help_text='')
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Obtener el tipo de usuario enviado en los argumentos del formulario
+        tipo_usuario = kwargs.get('initial', {}).get('tipo_usuario')
+        # Condición para mostrar las opciones dependiendo del tipo de usuario
+        if tipo_usuario == 'administrador':
+            # Opciones para el tipo de usuario administrador
+            self.fields['tipo_usuario'] = forms.ChoiceField(choices=(('user', 'User'), ('administrador', 'Administrador')))
+        else:
+            # Opciones para el tipo de usuario normal
+            self.fields['tipo_usuario'] = forms.ChoiceField(choices=(('user', 'User'),))
 
+    class Meta:
+        model = Perfil
+        fields = ['username', 'password', 'tipo_usuario']
+        
 class FormularioCancion(forms.ModelForm):
     class Meta:
         model = Cancion
         fields = ['titulo', 'artista', 'archivo_mp3', 'imagen']
 
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Nombre',
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
+    password = forms.CharField(
+        label='Contraseña',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'})
+    )
--- Proyecto/HeroSound/urls.py
+++ Proyecto/HeroSound/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from HeroSound import views
-from .views import start_music, cargar_music, busqueda, detalle_cancion
+from .views import start_music, cargar_music, busqueda, detalle_cancion, login_view, logout_view, registro, show_administrador
 
 
 urlpatterns = [
@@ -9,4 +9,8 @@ urlpatterns = [
     path('cargar_music/', views.cargar_music, name='cargar_music'),
     path('busqueda/', views.busqueda, name='busqueda'),  
     path('detalle_cancion/', views.detalle_cancion, name='detalle_cancion'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('registro/', views.registro, name='registro'),
+    path('administrador/', views.show_administrador, name='administrador'),    
 ]
